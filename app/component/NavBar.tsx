866814eace74b78ad776240c58528db3370e9707
Kavin24 <j.kavin2024@gmail.com>
2024-02-08 12:02:39 +0700

    nav fin

--- app/component/NavBar.tsx
+++ app/component/NavBar.tsx
@@ -12,7 +12,11 @@ function NavBar() {
       <div className="flex h-14  w-full flex-row flex-wrap items-center justify-between mxl:container mxl:mx-auto ">
         <h1 className=" text-2xl font-bold text-text-color">KVshop</h1>
         <ul className="me-auto ms-28 hidden lg:flex">
-          <li className="me-4 font-bold text-secondary-text-color">Home</li>
+          <li className="me-4 h-auto w-auto font-bold transition-all delay-75 duration-150 hover:scale-110 text-primary-color scale-105">
+            <Link className="p-2 " href="/">
+              Home
+            </Link>
+          </li>
           <li className="me-4 font-bold text-secondary-text-color">Porduct</li>
           <li className="me-4 font-bold text-secondary-text-color">Pricing</li>
           <li className="me-4 font-bold text-secondary-text-color">Contact</li>
@@ -33,15 +37,21 @@ function NavBar() {
           </li>
         </ul>
       </div>
-      <div className="bg-gray-50 absolute left-0 right-0 top-24 flex h-[450px] w-full items-center justify-center rounded-md opacity-80 shadow-sm transition-all delay-0 duration-500 ease-in-out md:hidden">
-        <ul className=" flex flex-col items-center justify-center text-center text-3xl text-secondary-text-color opacity-100 transition-all delay-300  duration-500 ease-in-out h-full w-full space-y-7">
+      <div className="absolute left-0 right-0 top-24 flex h-[450px] w-full items-center justify-center rounded-md bg-gray-50 opacity-80 shadow-sm transition-all delay-0 duration-500 ease-in-out md:hidden">
+        <ul className=" flex h-full w-full flex-col items-center justify-center space-y-7 text-center text-3xl text-secondary-text-color  opacity-100 transition-all delay-300 duration-500 ease-in-out">
           <li className="w-auto p-1">
-            <Link className="p-3 inline-block w-screen bg-orange-50 rounded-md text-black scale-110" href="/">
+            <Link
+              className="inline-block w-screen scale-110 rounded-md bg-orange-50 p-3 text-black"
+              href="/"
+            >
               Home
             </Link>
           </li>
           <li className="w-auto p-1">
-            <Link className="p-3 inline-block w-screen rounded-md text-black" href="/product">
+            <Link
+              className="inline-block w-screen rounded-md p-3 text-black"
+              href="/product"
+            >
               Product
             </Link>
           </li>
