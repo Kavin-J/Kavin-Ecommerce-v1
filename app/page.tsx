da1ab51bee6062eda7e4c1dcbf9f0dcf2428cbc2
Kavin24 <j.kavin2024@gmail.com>
2024-02-09 12:46:08 +0700

    hero 3

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
-import { Button } from "@nextui-org/react";
-import NavBar from "./component/NavBar";
-import Image from 'next/image'
+import Image from "next/image";
+
 function Page() {
   return (
     <main>
-      <div className="mxl:container mx-auto px-3 lg:px-9 bg-red-100 w-full h-screen">
-        <div className="w-full p-7 h-screen bg-sky-200 rounded-2xl bg-gradient-to-r from-gradient-s to-gradient-e">
-          <Image src='/hero-cover-1.svg' alt="KVshop Banner hero" width={696} height={619}/>
+      <div className="mx-auto h-screen w-full bg-red-100 px-3 mxl:container lg:px-9">
+        <div className="h-screen w-full rounded-2xl bg-sky-200 bg-gradient-to-r from-gradient-s to-gradient-e p-7">
+          {/* <Image
+            src="/hero-cover-1.svg"
+            alt="KVshop Banner hero"
+            width={696}
+            height={619}
+          /> */}
         </div>
-        
       </div>
     </main>
   );
